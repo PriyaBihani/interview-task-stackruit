b1966eec1d6ac2674418d79748d2073d6fb6d622
PriyaBihani <priyabihani.official@gmail.com>
2023-08-22 23:50:39 +0530

    express validator

--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,16 @@
 const { Router } = require('express');
+const { body } = require("express-validator");
 const { getPosts, createPost, getPost, updatePost, deletePost } = require('../controllers/post');
 const router = Router();
 
 router.get('/', getPosts);
 router.get('/:postId', getPost);
-router.post('/', createPost);
-router.patch('/:postId', updatePost);
+router.post('/', 
+    body("author").trim().not().isEmpty(),
+    body("title").trim().not().isEmpty(),
+    createPost
+);
 router.delete('/:postId', deletePost);
+router.patch('/:postId', updatePost);
 
 module.exports = router;
