eace9558731e1983649a5064e858b6aebf066c98
PriyaBihani <priyabihani.official@gmail.com>
2023-08-23 10:47:12 +0530

    "Enter validation"

---
+++ middlewares/validation.js
@@ -0,0 +1,13 @@
+const expressValidator = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        message: "Validation failed",
+        success: false,
+        data: errors.array(),
+      });
+    }
+    next();
+}
+
+module.exports = { expressValidator };
--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { body } = require("express-validator");
 const { getPosts, createPost, getPost, updatePost, deletePost } = require('../controllers/post');
+const { expressValidator } = require('../middlewares/validation');
 const router = Router();
 
 router.get('/', getPosts);
@@ -8,6 +9,7 @@ router.get('/:postId', getPost);
 router.post('/', 
     body("author").trim().not().isEmpty(),
     body("title").trim().not().isEmpty(),
+    expressValidator,
     createPost
 );
 router.delete('/:postId', deletePost);
